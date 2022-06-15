516241bbc4d1545a792c16ef99b3e8e5d1de4ab9
Ryan <ryanmahara@gmail.com>
2022-06-15 17:08:28 -0500

    Add Header, Content, and Total component to refactor App component

--- Part1/courseinfo/src/App.js
+++ Part1/courseinfo/src/App.js
@@ -1,3 +1,22 @@
+function Header({ title }) {
+  return <h1>{title}</h1>;
+}
+
+function Content({ contArr }) {
+  let contentArr = [];
+
+  for (let i = 0; i < contArr.length; i++) {
+    contentArr.push(<p>{contArr[i][0] + ' ' + contArr[i][1]}</p>);
+  }
+  return [...contentArr];
+}
+
+function Total({ exerciseArr }) {
+  return (
+    <p>Number of exercises {exerciseArr.reduce((acc, n) => acc + n, 0)}</p>
+  );
+}
+
 const App = () => {
   const course = 'Half Stack application development';
   const part1 = 'Fundamentals of React';
@@ -7,19 +26,17 @@ const App = () => {
   const part3 = 'State of a component';
   const exercises3 = 14;
 
+  let contentArray = [
+    [part1, exercises1],
+    [part2, exercises2],
+    [part3, exercises3],
+  ];
+
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header title={course} />
+      <Content contArr={contentArray} />
+      <Total exerciseArr={[exercises1, exercises2, exercises3]} />
     </div>
   );
 };
