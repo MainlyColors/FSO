a7db04bc2a380a11d5e1eef3b00cf296c0f14b9f
Ryan <ryanmahara@gmail.com>
2022-06-15 17:40:55 -0500

    refactor data variable into a single object

--- Part1/courseinfo/src/App.js
+++ Part1/courseinfo/src/App.js
@@ -26,27 +26,29 @@ function Total({ exerciseArr }) {
 }
 
 const App = () => {
-  const course = 'Half Stack application development';
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10,
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7,
-    },
-    {
-      name: 'State of a component',
-      exercises: 14,
-    },
-  ];
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+      },
+    ],
+  };
 
   return (
     <div>
-      <Header title={course} />
-      <Content contArr={parts} />
-      <Total exerciseArr={parts} />
+      <Header title={course.name} />
+      <Content contArr={course.parts} />
+      <Total exerciseArr={course.parts} />
     </div>
   );
 };
