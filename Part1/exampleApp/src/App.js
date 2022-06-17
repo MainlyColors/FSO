b7455d431ed9c9d1078138d8e1d5886c660aab53
Ryan <ryanmahara@gmail.com>
2022-06-17 15:40:56 -0500

    Add new JSX and bornYear calculation to Hello component

--- Part1/exampleApp/src/App.js
+++ Part1/exampleApp/src/App.js
@@ -1,20 +1,25 @@
-function Hello(props) {
+function Hello({ name, age }) {
+  const bornYear = () => new Date().getFullYear() - age;
+
   return (
     <div>
       <p>
-        Hello {props.name}, you are {props.age} years old
+        Hello {name}, you are {age} years old
       </p>
+      <p> So you were probably born in {bornYear()}</p>
+      <br />
     </div>
   );
 }
 
 function App() {
-  console.log('loaded');
+  const name = 'Peter';
+  const age = 10;
   return (
     <div>
       <h1>Greetings</h1>
-      <Hello name="ryan" age="24" />
-      <Hello name="kyle" />
+      <Hello name="Maya" age={26 + 10} />
+      <Hello name={name} age={age} />
     </div>
   );
 }
