485e427e5b17cd61c10666668597ea8d971d4289
Ryan <ryanmahara@gmail.com>
2022-06-22 17:52:41 -0500

    change APP component from setTimeOut based counter to button based

--- Part1/exampleApp/src/App.js
+++ Part1/exampleApp/src/App.js
@@ -4,10 +4,18 @@ function App() {
   // needs to be inside your component to work
   const [counter, setCounter] = useState(0);
 
-  setTimeout(() => setCounter(counter + 1), 1000);
+  const increaseByOne = () => setCounter(counter + 1);
+  const setToZero = () => setCounter(0);
 
   console.log('rendering...', counter);
-  return <div>{counter}</div>;
+
+  return (
+    <div>
+      <div>{counter}</div>
+      <button onClick={increaseByOne}>plus</button>
+      <button onClick={setToZero}>reset</button>
+    </div>
+  );
 }
 
 export default App;
