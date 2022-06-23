83696a62a9d676bc91e6d00c1947d865352e5eda
Ryan <ryanmahara@gmail.com>
2022-06-23 16:46:53 -0500

    exercise 1.6 done: add general html structure with state attached to buttons

--- Part1/unicafe/src/App.js
+++ Part1/unicafe/src/App.js
@@ -6,7 +6,25 @@ const App = () => {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  return <div>code here</div>;
+  return (
+    <>
+      <h1>give feedback</h1>
+      <button onClick={() => setGood(good + 1)}>good</button>
+      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+      <button onClick={() => setBad(bad + 1)}>bad</button>
+      <h2>statistics</h2>
+      <ul
+        style={{
+          listStyle: 'none',
+          padding: 0,
+        }}
+      >
+        <li>good:{good}</li>
+        <li>neutral:{neutral}</li>
+        <li>bad:{bad}</li>
+      </ul>
+    </>
+  );
 };
 
 export default App;
