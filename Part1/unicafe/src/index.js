dd418be2396d4b8d73fd9452ee1981d5756ea17d
Ryan <ryanmahara@gmail.com>
2022-06-23 16:33:27 -0500

    add unicafe project structure with starting code for exercise 1.6

---
+++ Part1/unicafe/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import App from './App';
+
+const roolEl = document.getElementById('root');
+
+const root = ReactDOM.createRoot(roolEl);
+
+root.render(<App />);
