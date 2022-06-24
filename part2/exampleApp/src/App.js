3befc4e178844ebbb5be51df680b677de6cb6d12
Ryan <ryanmahara@gmail.com>
2022-06-24 13:12:19 -0500

    add form element to app component for adding new notes

--- part2/exampleApp/src/App.js
+++ part2/exampleApp/src/App.js
@@ -1,6 +1,31 @@
+import { useState } from 'react';
+
 import Note from './components/Note';
 
-function App({ notes }) {
+function App({ notes: notesArr }) {
+  const [notes, setNotes] = useState(notesArr);
+  const [newNote, setNewNote] = useState('a new note...');
+
+  const addNoteHandler = (e) => {
+    e.preventDefault();
+    console.log('button clicked', e.target);
+
+    const noteObject = {
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() < 0.5,
+      id: notes.length + 1,
+    };
+
+    setNotes(notes.concat(noteObject));
+    setNewNote('');
+  };
+
+  const noteChangeHandler = (e) => {
+    console.log(e.target);
+    setNewNote(e.target.value);
+  };
+
   return (
     <div>
       <h1>Notes</h1>
@@ -9,6 +34,10 @@ function App({ notes }) {
           <Note key={note.id} note={note} />
         ))}
       </ul>
+      <form onSubmit={addNoteHandler}>
+        <input value={newNote} onChange={noteChangeHandler} />
+        <button type="submit">save</button>
+      </form>
     </div>
   );
 }
