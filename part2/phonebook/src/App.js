3aa61a2b5363aad4208d0438d610fc375e9dac82
Ryan <ryanmahara@gmail.com>
2022-06-24 15:52:28 -0500

    complete exercise 2.6: add form and onchange handler for form element

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -4,19 +4,33 @@ const App = () => {
   const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]);
   const [newName, setNewName] = useState('');
 
+  function formHandler(e) {
+    e.preventDefault();
+    console.log(e.target);
+    setPersons(persons.concat({ name: newName }));
+  }
+
+  function onChangeHandler(e) {
+    console.log('changing', e.target);
+    setNewName(e.target.value);
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={formHandler}>
         <div>
-          name: <input />
+          <label htmlFor="name">name:</label>
+          <input id="name" value={newName} onChange={onChangeHandler} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      {persons.map((person) => (
+        <p key={person.name}>{person.name}</p>
+      ))}
     </div>
   );
 };
