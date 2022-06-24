52311fc0c47bfa64a59b0a0510608fccea83a638
Ryan <ryanmahara@gmail.com>
2022-06-24 16:33:29 -0500

    complete exercise 2.9: add filter feature to phonebook

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,8 +2,12 @@ import { useState } from 'react';
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '815-637-1258' },
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
   ]);
+  const [newFilter, setNewFilter] = useState('');
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
 
@@ -34,9 +38,27 @@ const App = () => {
     setNewNumber(e.target.value);
   }
 
+  function filterOnChangeHandler(e) {
+    console.log('changing', e.target);
+    setNewFilter(e.target.value.toLowerCase());
+  }
+
+  const personsFilterArr =
+    newFilter === ''
+      ? persons
+      : persons.filter((person) =>
+          person.name.toLowerCase().includes(newFilter)
+        );
+
   return (
     <div>
-      <h2>Phonebook</h2>
+      <h1>Phonebook</h1>
+
+      <div>
+        <label htmlFor="filter">filter shown with:</label>
+        <input id="filter" value={newFilter} onChange={filterOnChangeHandler} />
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={formHandler}>
         <div>
           <label htmlFor="name">name:</label>
@@ -55,7 +77,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person) => (
+      {personsFilterArr.map((person) => (
         <p key={person.name}>
           {person.name} {person.number}
         </p>
