7419cb8a06d059649bae48f69a16fc34ede0ea9e
Ryan <ryanmahara@gmail.com>
2022-06-24 12:04:10 -0500

    complete exercise 2.3-2.4: Add more data to courses array and display it in App component

--- Part1/courseinfo/src/App.js
+++ Part1/courseinfo/src/App.js
@@ -1,5 +1,5 @@
 function Header({ title }) {
-  return <h1>{title}</h1>;
+  return <h2>{title}</h2>;
 }
 
 function Content({ content }) {
@@ -31,32 +31,61 @@ function Course({ course }) {
 }
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1,
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2,
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3,
-      },
-    ],
-  };
-
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1,
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2,
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3,
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4,
+        },
+      ],
+    },
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1,
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2,
+        },
+      ],
+    },
+  ];
   return (
     <>
-      <Course course={course} />
-      <Total parts={course.parts} />
+      <h1>Web development curriculum</h1>
+      <section>
+        <Course course={courses[0]} />
+        <Total parts={courses[0].parts} />
+      </section>
+      <section>
+        <Course course={courses[1]} />
+        <Total parts={courses[1].parts} />
+      </section>
     </>
   );
 };
